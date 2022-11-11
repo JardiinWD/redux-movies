41cea1af88188460d2541f1c7bb7de946f41032c
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-11-11 21:19:58 +0100

    #8 - I've created the search bar. Now I'll can implement the filtering of movies or series.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/Header.scss'
 import User from '../images/user.png'
 import { Link } from 'react-router-dom'
@@ -6,13 +6,34 @@ import { Link } from 'react-router-dom'
 
 
 const Header = () => {
+
+    // Text state for the search-bar
+    const [term, setTerm] = useState("")
+
+    // submitHandler Function
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log(term);
+    }
+
     return (
         /* header */
         <div className="header">
-            <Link to="/">
-                {/* logo */}
-                <div className="logo">Movie App</div>
-            </Link>
+            {/* logo */}
+            <div className="logo">
+                <Link to="/">Movie App</Link>
+            </div>
+            {/* search-bar */}
+            <div className="search-bar">
+                <form onSubmit={submitHandler}>
+                    <input type="text" value={term} placeholder="Search Movies or Series..." onChange={(e) => setTerm(e.target.value)} />
+                    {/* submit button */}
+                    <button type='submit'>
+                        {/* fa fa-search */}
+                        <i className="fa fa-search"></i>
+                    </button>
+                </form>
+            </div>
             {/* user-image */}
             <div className="user-image">
                 <img src={User} alt="user" />
